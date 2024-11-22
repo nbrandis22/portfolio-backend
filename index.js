b097e124d734b158d8553904d5e3bd9246911276
Nick Brandis <nick.brandis@teradata.com>
2024-11-22 14:52:12 -0800

    added cors and express.json

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 
 const app = express();
 const PORT = 4000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
